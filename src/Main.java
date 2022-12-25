29edd0dfdc7ba91edc00be42f33fd2d14af4eb12
mengwangusc <wangmeng@usc.edu>
2022-12-25 11:50:11 -0800

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int i = 0;
+        System.out.println("test" + i);
         System.out.println("Hello world!");
+        int j = 1;
+        System.out.println("test" + j);
     }
 }
